433bc0b0134054d10ed0e851ff32dfbf4650e8bc
chopperhl <871256hl>
2018-12-23 06:30:40 +0800

    server

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class Home extends React.Component {
     render() {
-        return (<div>This is Home</div>)
+        return (<div>This is Home!</div>)
     }
 
 }
