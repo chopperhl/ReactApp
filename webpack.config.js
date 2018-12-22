3ca412a6a49eef249a646a3e5a2dc9f38adf0bd9
chopperhl <871256hl>
2018-12-23 05:51:29 +0800

    html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,21 @@ const path = require('path');
 
 
 const config = {
+    mode: "development",
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    resolve: {
+        extensions: [".js", ".jsx", ".css"],
+    },
     module: {
         rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
             {
                 test: /\.js|jsx$/, use: [{
                     loader: "babel-loader",
